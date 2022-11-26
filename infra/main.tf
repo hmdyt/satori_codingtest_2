a308f9639196346c251c226d13cf8bad673906ab
hmdyt <konnyaku108@gmail.com>
2022-11-26 19:02:37 +0900

    actions

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket = "satori-codingtest-2-tfstete"
+    prefix = "terraform/state"
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region

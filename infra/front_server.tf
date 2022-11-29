5545e979494ec1c6b992deed13d097c03b401403
hmdyt <konnyaku108@gmail.com>
2022-11-29 22:09:09 +0900

    外からのアクセスを制限

--- infra/front_server.tf
+++ infra/front_server.tf
@@ -1,6 +1,7 @@
 resource "google_cloud_run_service" "web" {
-  name     = "web"
-  location = var.region
+  name       = "web"
+  location   = var.region
+  depends_on = [google_service_networking_connection.vpc_connection]
 
   template {
     spec {
@@ -9,21 +10,4 @@ resource "google_cloud_run_service" "web" {
       }
     }
   }
-}
-
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location = google_cloud_run_service.web.location
-  project  = google_cloud_run_service.web.project
-  service  = google_cloud_run_service.web.name
-
-  policy_data = data.google_iam_policy.noauth.policy_data
 }

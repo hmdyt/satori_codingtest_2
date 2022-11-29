57f7ed9a252accfcc1aef018d2e1ec3cb19a794f
hmdyt <konnyaku108@gmail.com>
2022-11-29 22:51:03 +0900

    fixup! api serverからnextへproxyするように

--- infra/front_server.tf
+++ infra/front_server.tf
@@ -11,3 +11,20 @@ resource "google_cloud_run_service" "web" {
     }
   }
 }
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.web.location
+  project  = google_cloud_run_service.web.project
+  service  = google_cloud_run_service.web.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}

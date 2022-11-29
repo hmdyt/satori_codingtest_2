34013b3527bc24758fb5163e1a9f1dc7a6764e57
hmdyt <konnyaku108@gmail.com>
2022-11-29 21:58:23 +0900

    health check

--- api/app.go
+++ api/app.go
@@ -16,9 +16,11 @@ var router *mux.Router
 func init() {
 	router = Router()
 	functions.HTTP("hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("called hello")
+
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		
+
 		w.Header().Set("Access-Control-Allow-Methods", "*")
 		w.Header().Set("Access-Control-Request-Method", "*")
 		router.ServeHTTP(w, r)
